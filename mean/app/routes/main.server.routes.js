9c30ad56f58f14b1747e5d73f12cf74e149ce2d0
Sreedhar Boggarapu <sreedhar.boggarapu@octoconsulting.com>
2015-12-20 18:54:41 -0500

    Initial Project Structure
    Initial Project Structure

---
+++ mean/app/routes/main.server.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var main = require('../../app/controllers/main.server.controller');
+
+module.exports = function(app) {
+console.log('In autocomplete');
+	app.route('/inflo/api/autoComplete')
+		.get(main.autoComplete);	
+
+	app.route('/inflo/api/search')
+		.get(main.searchByCountyState);		
+
+};
